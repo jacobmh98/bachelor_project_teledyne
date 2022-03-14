e19a22bcba502e0c91c490c81ddc46dc6e949529
jacobmh98 <jacobmelchiorhansen98@gmail.com>
2022-03-14 14:57:28 +0100

    coordinates for each ping NOT connecting multiple pings

--- cartesian_coordinates.py
+++ cartesian_coordinates.py
@@ -16,16 +16,29 @@ base_x = R * cos(base_lat) * cos(base_long)
 base_y = R * cos(base_lat) * sin(base_long)
 base_z = R * sin(base_lat)
 
-# Setting cartesian coordinates for each ping location with ping0 as 0-reference
-ping_coords = [(0,0)]
+#
+ping_coords = [(0, 0, 0)]
+ping_point_rows = []
 for p in dataset:
+    # Setting cartesian coordinates for each ping location with ping0 as 0-reference
     if p != ping0:
         p_lat = p.position_set[0].latitude
         p_long = p.position_set[0].longitude
         p_x = R * cos(p_lat) * cos(p_long) - base_x
         p_y = R * cos(p_lat) * sin(p_long) - base_y
         #p_z = R * sin(p_lat) - base_z
-        ping_coords.append((p_x, p_y))
+        ping_coords.append((p_x, p_y, 0))
 
-for i, c in enumerate(ping_coords):
-    print(f"(x{i}, y{i}) = {c}")
+    # Setting cartesian coordinates for all points in a ping
+    p_points = []
+    for d in p.raw_detections.detections:
+        length = d[1]
+        angle = d[2]
+
+        p_x, p_y, p_z = ping_coords[-1]
+        point_y = p_y + sin(angle) * length
+        point_z = -(p_z + cos(angle) * length)
+        p_points.append((p_x, point_y, point_z))
+    ping_point_rows.append(p_points)
+
+# TODO account for changing of basis vectors when boat rotates (VERY IMPORTANT)
